a0b59593af41f9f9e96bf6d8fae2ce3003499198
novaez <4547366+novaez@users.noreply.github.com>
2019-09-10 20:06:47 +0800

    Make letencrypt-create.sh totally no interception.
    * -n, Run non-interactively
    * --manual-public-ip-logging-ok,  Automatically allows public IP logging (default: Ask)

--- letsencrypt-create.sh
+++ letsencrypt-create.sh
@@ -17,26 +17,26 @@ while getopts "m:d:f" opt; do
       FORCE=1
       ;;
     \?)
-      echo "Invalid option: -$OPTARG" 
+      echo "Invalid option: -$OPTARG"
       exit 1
       ;;
   esac
 done
 
-if [ -z "${DOMAIN}" ]; then 
+if [ -z "${DOMAIN}" ]; then
   echo "Option:-d is necessary."
   exit 1
 fi
 
 cmd="certbot certonly"
-if [ -n "${EMAIL}" ]; then 
+if [ -n "${EMAIL}" ]; then
   cmd="${cmd} --email ${EMAIL} "
 fi
 
-if [ -n "${FORCE}" ]; then 
+if [ -n "${FORCE}" ]; then
   cmd="${cmd} --force-renewal "
 fi
 
-cmd="${cmd} --agree-tos --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory --manual --manual-auth-hook 'python $DIR/manual-hook.py --auth' --manual-cleanup-hook 'python $DIR/manual-hook.py --cleanup' -d ${DOMAIN} -d *.${DOMAIN}"
+cmd="${cmd} -n --manual-public-ip-logging-ok --agree-tos --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory --manual --manual-auth-hook 'python $DIR/manual-hook.py --auth' --manual-cleanup-hook 'python $DIR/manual-hook.py --cleanup' -d ${DOMAIN} -d *.${DOMAIN}"
 
 eval ${cmd}
